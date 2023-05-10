febec1a948e2bb595830cb003611ec8a4916095c
Buck3ts41 <107511158+Buck3ts41@users.noreply.github.com>
2023-05-10 17:46:42 +0200

    offsets.h 2.8.1

--- SoT_External_Tool_Premium/Core/offsets.h
+++ SoT_External_Tool_Premium/Core/offsets.h
@@ -3,13 +3,13 @@ class offsets {
 public:
 	struct UWorld
 	{
-		int PersistentLevel = 48;
-		int OwningGameInstance = 448;
+		int PersistentLevel = 0x30;
+		int OwningGameInstance = 0x1C0;
 	}UWorld;
 
 	struct UGameInstance
 	{
-		int LocalPlayers = 56;
+		int LocalPlayers = 0x38;
 	}UGameInstance;
 
 	struct UPlayer
@@ -24,83 +24,83 @@ public:
 
 	struct APlayerController
 	{
-		int Pawn = 976;
-		int Character = 992;
-		int CameraManager = 1112;
+		int Pawn = 0x3D0;
+		int Character = 0x3E0;
+		int CameraManager = 0x458;
 		int ControlRotation = 772;
 	}APlayerController;
 
 	struct AActor
 	{
-		int actorId = 24;
-		int rootComponent = 360;
-		int PlayerState = 1000;
-		int WieldedItemComponent = 2096;
-		int HealthComponent = 2200;
-		int DrowningComponent = 3472;
+		int actorId = 0x18;
+		int rootComponent = 0x168;
+		int PlayerState = 0x3E8;
+		int WieldedItemComponent = 0x870;
+		int HealthComponent = 0x898;
+		int DrowningComponent = 0xD98;
 	}AActor;
 
 	struct APlayerState
 	{
-		int PlayerName = 976;
+		int PlayerName = 0x3D0;
 	}APlayerState;
 
 	struct APlayerCameraManager
 	{
-		int CameraCache = 1088;
+		int CameraCache = 0x440;
 	}APlayerCameraManager;
 
 	struct AItemProxy
 	{
-		int AItemInfo = 1816;
+		int AItemInfo = 0x5C0;
 	}AItemProxy;
 
 	struct AItemInfo
 	{
-		int UItemDesc = 1088;
+		int UItemDesc = 0x440;
 	}AItemInfo;
 
 	struct ABootyItemInfo
 	{
-		int BootyType = 1344;
-		int Rarity = 1716;
+		int BootyType = 0x540;
+		int Rarity = 0x6B4;
 	}ABootyItemInfo;
 
 	struct AShip
 	{
-		int CrewOwnershipComponent = 1968;
-		int ShipInternalWaterComponent = 1568;
-		int ShipOwningActor = 1000;
+		int CrewOwnershipComponent = 0x7B0;
+		int ShipInternalWaterComponent = 0x620;
+		int ShipOwningActor = 0x3E8;
 	}AShip;
 
 	struct ACannon
 	{
-		int TimePerFire = 1464;
-		int ProjectileSpeed = 1468;
-		int ProjectileGravityScale = 1472;
+		int TimePerFire = 0x5B8;
+		int ProjectileSpeed = 0x5BC;
+		int ProjectileGravityScale = 0x5C0;
 		int ServerPitch = 1976;
 	}ACannon;
 
 	struct UCrewOwnershipComponent
 	{
-		int CrewId = 212;
+		int CrewId = 0xD4;
 	}UCrewOwnershipComponent;
 
 	struct AFauna
 	{
-		int Name = 2072;
+		int Name = 0x818;
 	}AFauna;
 
 	struct AMaptTable
 	{
-		int ServerCenter = 1224;
-		int MapPins = 1256;
-		int TrackedShips = 1272;
+		int ServerCenter = 0x4C8;
+		int MapPins = 0x4E8;
+		int TrackedShips = 0x4F8;
 	}AMapTable;
 
 	struct ACrewService
 	{
-		int Crews = 1184;
+		int Crews = 0x4A0;
 	}ACrewService;
 
 	struct UWieldedItemComponent
